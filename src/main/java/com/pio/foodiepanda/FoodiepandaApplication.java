d60ce6d181b63501a898bb154667889c1fb73645
Madhu Shekhawat <madhu.shekhawat@programmers.io>
2025-01-02 12:34:33 +0530

    initial commit

---
+++ src/main/java/com/pio/foodiepanda/FoodiepandaApplication.java
@@ -0,0 +1,13 @@
+package com.pio.foodiepanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiepandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiepandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pio/foodiepanda/FoodiepandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pio.foodiepanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiepandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
